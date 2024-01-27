6f813c96fd463435b18a7cad65a028da7c8580bc
David Park <d12ap150@yahoo.com>
2024-01-27 14:33:55 -0700

    cancel button

--- front-end/src/dashboard/reservationTable/ReservationRow.js
+++ front-end/src/dashboard/reservationTable/ReservationRow.js
@@ -41,15 +41,7 @@ export default function ReservationRow({ reservation, cancelRes }) {
           }
         </td>
         <td>
-        <button
-                      type="button"
-                      className="btn btn-danger mx-2 text-white"
-                      data-reservation-id-cancel={reservation.reservation_id}
-                      value={reservation.reservation_id}
-                      onClick={handleCancel}
-                    >
-                      Cancel
-          </button>
+        <button type="button" onClick={() => history.goBack()}>Cancel</button>
         </td>
     </tr>
   );
